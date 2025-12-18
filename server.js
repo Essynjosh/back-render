bc441e647562abcbc7c7a704104e70c9097d02d8
Essynjosh <essynjosh353@gmail.com>
2025-12-18 20:24:09 +0300

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: [
-      "https://front-vercel-8gba70mld-essynjosh353-gmailcoms-projects.vercel.app",
+      "https://front-vercel-omega.vercel.app",
     ],
     methods: ["GET", "POST"],
   })
